72469d57f8ef6be9000adb2cc5d2504a3070687f
Andres Norato <carnorat@gmail.com>
2022-09-13 16:10:42 -0500

    rutas en node

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('My app');
+//Importamos a express recordemos que express es un mini framework que corre en node.js
+const express = require('express');
+
+//Creamos la variable y  le damos la instacia de express lo que nos crea la app
+const app = express();
+
+//Definimos un puerto por donde vamos a escuchar
+const port = 3001;
+
+
+app.get('/', (req, res) => {
+  res.send('Hola mi serve en express');
+});
+
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola soy la nueva ruta');
+});
+
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  });
+});
+
+
+app.get('/categories', (req, res) =>{
+  res.json({
+    id: 1,
+    name: 'Eletrodomesticos',
+  })
+})
+
+
+app.listen(port, ()=> {
+  console.log('Mi port' + port);
+});
