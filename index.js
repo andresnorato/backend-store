c3a1d7e7cd52a5316a75397a00d7cd2ef9662607
Andres Norato <carnorat@gmail.com>
2022-09-21 12:24:47 -0500

    ruta maestra de versionamiento

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routerApi =  require('./routes');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 //ROUTING EN EXPRESS
 
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 });
 
 
-routerApi(app)
+routerApi(app);
 
 
 app.listen(port, () => {
